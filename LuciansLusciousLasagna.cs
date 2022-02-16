e1946ff05714fc132e8f329c90aa929980e4a1bb
Sami0412 <samiw0412@gmail.com>
2022-02-16 16:37:49 +0000

    First solution attempt

--- LuciansLusciousLasagna.cs
+++ LuciansLusciousLasagna.cs
@@ -1,10 +1,33 @@
 class Lasagna
 {
+    private const int OvenTime = 40;
+
     // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven()
+    {
+        return OvenTime;
+    }
 
     // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int minsPassed)
+    {
+        var minsRemaining = OvenTime - minsPassed;
+        return minsRemaining;
+    }
 
     // TODO: define the 'PreparationTimeInMinutes()' method
-
+    public int PreparationTimeInMinutes(int layers)
+    {
+        var minsPerLayer = 2;
+        var totalPrepTime = layers * minsPerLayer;
+        return totalPrepTime;
+    }
     // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int layers, int minsPassed)
+    {
+        var minsPerLayer = 2;
+        var totalPrepTime = layers * minsPerLayer;
+        var elapsedTime = totalPrepTime + minsPassed;
+        return elapsedTime;
+    }
 }
